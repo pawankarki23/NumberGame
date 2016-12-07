adaf5a0f025e3ca7c0dad1d6537ac4f76d5ff852
Pawan Karki <pawankarki5@gmail.com>
2016-12-06 19:03:19 -0500

    added the table view and new view controller

--- NumberGame/ViewController.swift
+++ NumberGame/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var numberTable: UITableView!
+    
+    var numbers = [1,2,3,4,5,6,7,8,9,10]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //
+        numberTable.dataSource = self
+        numberTable.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (numbers.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = String(numbers[indexPath.row])
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
